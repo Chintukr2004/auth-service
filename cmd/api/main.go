c82a0c36e1cf09b7ca1ed028358007155a20de0b
Chintukr2004 <chintukr1904@gmail.com>
2026-03-29 15:47:24 +0530

    Add config management using env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,20 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Chintukr2004/auth-service/internal/config"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+
+	cfg := config.LoadConfig()
+
 	r := chi.NewRouter()
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("server is running"))
+		w.Write([]byte("server is running on port" + cfg.Port))
 	})
 
-	port := ":8080"
-	log.Println("server runnnig on port", port)
+	log.Println("server runnnig on port", cfg.Port)
 
-	err := http.ListenAndServe(port, r)
+	err := http.ListenAndServe(":"+cfg.Port, r)
 	if err != nil {
 		log.Fatal("server failed", err)
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+
+	DBHost     string
+	DBPort     string
+	DBuser     string
+	DBPassword string
+	DBName     string
+
+	JWTSecret string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system env")
+	}
+
+	return &Config{
+		Port:       getEnv("PORT", "8080"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_USER", "postgres"),
+		DBuser:     getEnv("DB_USER", "postges"),
+		DBName:     getEnv("DB_NAME", "auth_db"),
+		DBPassword: getEnv("DB_PASSWORD", "secret"),
+		JWTSecret:  getEnv("JWT_SECRET", "secret"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
