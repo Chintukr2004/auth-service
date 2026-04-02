86729681055099c91421655950b3bc82e3285d93
Chintukr2004 <chintukr1904@gmail.com>
2026-04-02 21:22:49 +0530

    fix Railway DB connection suing DATABASE_URL

--- internal/database/db.go
+++ internal/database/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/Chintukr2004/auth-service/internal/config"
@@ -11,20 +12,24 @@ import (
 )
 
 func NewPostgresDB(cfg *config.Config) *pgxpool.Pool {
-	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		cfg.DBUser,
-		cfg.DBPassword,
-		cfg.DBHost,
-		cfg.DBPort,
-		cfg.DBName,
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s?sslmode=require",
+			cfg.DBUser,
+			cfg.DBPassword,
+			cfg.DBHost,
+			cfg.DBPort,
+			cfg.DBName,
+		)
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	db, err := pgxpool.New(ctx, dsn)
 	if err != nil {
-		log.Fatal("Unable to connect to database:", err)
+		log.Println("Unable to connect to database:", err)
 	}
 
 	err = db.Ping(ctx)
