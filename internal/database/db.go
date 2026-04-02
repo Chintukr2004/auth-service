68e16541fd22f7d55d04dbda3a578a36c0ebeb63
Chintukr2004 <chintukr1904@gmail.com>
2026-04-02 21:52:07 +0530

    fix: clean DATABASE_URL config for Railway

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,26 +2,17 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
-	"github.com/Chintukr2004/auth-service/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewPostgresDB(cfg *config.Config) *pgxpool.Pool {
+func NewPostgresDB() *pgxpool.Pool {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = fmt.Sprintf(
-			"postgres://%s:%s@%s:%s/%s?sslmode=require",
-			cfg.DBUser,
-			cfg.DBPassword,
-			cfg.DBHost,
-			cfg.DBPort,
-			cfg.DBName,
-		)
+		log.Fatal("DATABASE_URL is not set")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -29,13 +20,13 @@ func NewPostgresDB(cfg *config.Config) *pgxpool.Pool {
 
 	db, err := pgxpool.New(ctx, dsn)
 	if err != nil {
-		log.Println("Unable to connect to database:", err)
+		log.Fatal("Failed to connect to DB:", err)
 	}
 
 	err = db.Ping(ctx)
 	if err != nil {
 		log.Fatal("Database ping failed:", err)
 	}
-	log.Println("Connected to postgreSql")
+	log.Println("Connected to postgreSQL")
 	return db
 }
